063dbf3053f513c3647f8528cdfe8a03d42c8649
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2022-06-02 17:33:42 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="Author" content="CyberMajeed" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="./assets/Neuron.png" type="image/png" />
+    <link rel="stylesheet" href="./css/index.css" />
+    <title>Neuron</title>
+  </head>
+  <body></body>
+</html>
