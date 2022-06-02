627850cf9884eda596aa2e25c6d1d70cc1924064
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2022-06-02 17:34:52 +0400

    username

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="./assets/Neuron.png" type="image/png" />
-    <link rel="stylesheet" href="./css/index.css" />
-    <title>Neuron</title>
+    <link rel="shortcut icon" href="./assets/Neuron.png" />
+    <link rel="stylesheet" href="./style/index.css" />
+    <title>Neuron - <?php echo "Username"?></title>
   </head>
   <body></body>
 </html>
